4cfb38fecb47b6eefebdb454df2fd4ce9850d58c
nipun <nipun.s@solutelabs.com>
2022-04-03 22:41:26 +0530

    New Code DP | String

--- Interview Que/GeekyAnt/R2_Pair_to_Target.py
+++ Interview Que/GeekyAnt/R2_Pair_to_Target.py
@@ -6,3 +6,25 @@ Que :- Array Provided, Find Pair of Index that Sum up to target vals
          - array is not sorted
 '''
 
+import itertools as it
+
+a = [2, 11, 15, 7]  # inp arr
+t = 18  # target
+
+# S1 sort the arr (O(nlogn))
+#a.sort()
+
+# S2. Discard candidates  ( O(n))
+#na = set(it.takewhile(lambda x: x <= t, a))
+na = {e for e in a if e <= t}
+
+# S3. 
+e1, e2 = -1, -1
+for e in na:
+    r = t-e
+    if r in na:
+        e1, e2 = e, r
+        
+i1, i2 = a.index(e1), a.index(e2)
+
+print(i1, i2)
